24448a353ac27e32972b899be7bf5c61f093cb92
evanharris <evan.tyler.harris@gmail.com>
2016-08-05 00:07:03 -0700

    Ability to change chart content

--- src/main/java/com/wbdev/obdhud/controller/ObdMetricsController.java
+++ src/main/java/com/wbdev/obdhud/controller/ObdMetricsController.java
@@ -5,7 +5,10 @@ import com.wbdev.obdhud.api.Metric;
 import com.wbdev.obdhud.exceptionhandling.NotConnectedException;
 import com.wbdev.obdhud.metrics.MetricService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.LinkedList;
